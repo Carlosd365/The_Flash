e99f3a6f8be7d14841a9e47453130156d5d1d669
Carlos Gómez <carlitosgoag3@gmail.com>
2023-02-14 09:56:25 -0600

    Modifique
    gitignore

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 print('Nombre: Carlos Daniel Gómez Aguilar, Carné: 1506323.')
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
